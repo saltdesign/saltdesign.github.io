9ffe7d76ed025c5772b9ee462aed61e05dc33d23
saltdesign <35103482+saltdesign@users.noreply.github.com>
2018-03-02 16:14:31 +0000

    SALTDESIGN

---
+++ js/index.js
@@ -0,0 +1,44 @@
+$('.menu-toggle').click(function() {
+  
+  $('.site-nav').toggleClass('site-nav--open', 500);
+  $(this).toggleClass('open');
+  
+})
+
+
+
+
+$(".to-home").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#home").offset().top
+    }, 2000);
+});
+
+$(".to-about").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".about-anchor").offset().top
+    }, 2000);
+});
+
+$(".to-portfolio").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#portfolio").offset().top
+    }, 2000);
+});
+
+$(".to-contact").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 2000);
+});
+
+
+
+
+
+$(document).ready(function(){
+    $("#show").click(function(){
+        $("h2").show();
+        this.style.display = 'none'
+    });
+});
